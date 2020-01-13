466d37c937595ced127df23514f661fbee824ba8
RockyAZ <33132543+RockyAZ@users.noreply.github.com>
2020-01-13 19:19:16 +0200

    initial commit

---
+++ Assets/Scripts/ScriptabldeObjects/EnemyData.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New EnemyData", menuName = "Enemy Data", order = 51)]
+public class EnemyData : ScriptableObject
+{
+	[SerializeField]
+	private int startHealth;
+	[SerializeField]
+	private float speed;
+	[SerializeField]
+	private int damage;
+	[SerializeField]
+	private GameObject model;
+	[SerializeField]
+	private float scale;
+
+	public int StartHealth { get => startHealth; }
+	public float Speed { get => speed; }
+	public int Damage { get => damage; }
+}
